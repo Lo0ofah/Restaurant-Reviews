fae5ecce6f8cc653d7ffe5bde517e6acc545b9e4
Elaaf Zaker <miss.lo0ofah@gmail.com>
2018-05-22 15:37:55 +0300

    feat: offline website

---
+++ sw/sw.js
@@ -0,0 +1,55 @@
+var staticCacheName = 'Restaurant-v1';
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(function(cache){
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js'
+      ]);
+    })
+  );
+    console.log("install");
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('Restaurant-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+    console.log("activate");
+});
+
+self.addEventListener("fetch", function(event){
+  event.respondWith(
+  caches.match(event.request)
+  .then(function(response) {
+    return response || fetch(event.request);
+  })
+);
+  console.log("fetch");
+});
